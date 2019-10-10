47aea5ba95de11619631ef0d2c8149fd1fca7ba3
Nishad Aherrao <niaherrao@MB-NIAHERRAO.local>
2019-10-10 12:08:09 -0500

    Added basic skeleton structure for how the page should look like with mock data

--- client/src/views/events/events.js
+++ client/src/views/events/events.js
@@ -1,10 +1,66 @@
 import React, { Component } from 'react'
-
+import { Button, Icon, Image, Item, Label } from 'semantic-ui-react'
+const paragraph = <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
 export default class Events extends Component {
   render() {
     return (
       <div>
-        <h1>Events</h1>
+        <Item.Group divided relaxed style={{ display: 'grid', justifyContent: 'center', paddingTop: '50px' }}>
+          <Item>
+            <Item.Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+
+            <Item.Content>
+              <Item.Header>Meeting Topic is ABC</Item.Header>
+              <Item.Meta>
+                <span>AT ERB 315</span>
+              </Item.Meta>
+              <Item.Description>{paragraph}</Item.Description>
+              <Item.Extra>
+                <Button primary floated="right">
+                  RSVP
+                  <Icon name="right chevron" />
+                </Button>
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+
+          <Item>
+            <Item.Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+
+            <Item.Content>
+              <Item.Header>Meeting Topic is DEF</Item.Header>
+              <Item.Meta>
+                <span>AT ERB 315</span>
+              </Item.Meta>
+              <Item.Description>{paragraph}</Item.Description>
+              <Item.Extra>
+                <Button primary floated="right">
+                  RSVP
+                  <Icon name="right chevron" />
+                </Button>
+                <Label>Speaker at Event</Label>
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+
+          <Item>
+            <Item.Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+
+            <Item.Content>
+              <Item.Header>Meeting Topic is XYZ</Item.Header>
+              <Item.Meta>
+                <span>AT ERB 315</span>
+              </Item.Meta>
+              <Item.Description>{paragraph}</Item.Description>
+              <Item.Extra>
+                <Button primary floated="right">
+                  RSVP
+                  <Icon name="right chevron" />
+                </Button>
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        </Item.Group>
       </div>
     )
   }
