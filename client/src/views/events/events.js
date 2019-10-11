035eb181bfc34fa2b8586acd96b2231b96139216
Nishad Aherrao <niaherrao@MB-NIAHERRAO.local>
2019-10-11 09:54:04 -0500

    Changed image to date in events

--- client/src/views/events/events.js
+++ client/src/views/events/events.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
-import { Button, Icon, Image, Item, Label } from 'semantic-ui-react'
+import { Button, Icon, Image, Item, Label, Card } from 'semantic-ui-react'
 const paragraph = <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
 export default class Events extends Component {
   render() {
     return (
       <div>
         <Item.Group divided relaxed style={{ display: 'grid', justifyContent: 'center', paddingTop: '50px' }}>
-          <Item>
-            <Item.Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-
+          <Item style={{ display: 'flex', justifyContent: 'center', paddingTop: '50px' }}>
+            <Item.Content
+              style={{ alignSelf: 'center', textAlign: 'center', fontSize: 'xx-large', paddingRight: '2vw' }}
+            >
+              <Item.Header>11</Item.Header>
+              <Item.Description>October</Item.Description>
+            </Item.Content>
             <Item.Content>
               <Item.Header>Meeting Topic is ABC</Item.Header>
               <Item.Meta>
@@ -24,11 +28,15 @@ export default class Events extends Component {
             </Item.Content>
           </Item>
 
-          <Item>
-            <Item.Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-
+          <Item style={{ display: 'flex', justifyContent: 'center', paddingTop: '50px' }}>
+            <Item.Content
+              style={{ alignSelf: 'center', textAlign: 'center', fontSize: 'xx-large', paddingRight: '2vw' }}
+            >
+              <Item.Header>11</Item.Header>
+              <Item.Description>October</Item.Description>
+            </Item.Content>
             <Item.Content>
-              <Item.Header>Meeting Topic is DEF</Item.Header>
+              <Item.Header>Meeting Topic is ABC</Item.Header>
               <Item.Meta>
                 <span>AT ERB 315</span>
               </Item.Meta>
@@ -38,16 +46,19 @@ export default class Events extends Component {
                   RSVP
                   <Icon name="right chevron" />
                 </Button>
-                <Label>Speaker at Event</Label>
               </Item.Extra>
             </Item.Content>
           </Item>
 
-          <Item>
-            <Item.Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
-
+          <Item style={{ display: 'flex', justifyContent: 'center', paddingTop: '50px' }}>
+            <Item.Content
+              style={{ alignSelf: 'center', textAlign: 'center', fontSize: 'xx-large', paddingRight: '2vw' }}
+            >
+              <Item.Header>11</Item.Header>
+              <Item.Description>October</Item.Description>
+            </Item.Content>
             <Item.Content>
-              <Item.Header>Meeting Topic is XYZ</Item.Header>
+              <Item.Header>Meeting Topic is ABC</Item.Header>
               <Item.Meta>
                 <span>AT ERB 315</span>
               </Item.Meta>
