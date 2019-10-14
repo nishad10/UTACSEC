5a4c97eef1636f6b26cb8c151674d056cbfe1df0
Nishad Aherrao <niaherrao@MB-NIAHERRAO.local>
2019-10-14 15:22:09 -0500

    changed eventbrite embed to modal

--- client/src/views/events/events.js
+++ client/src/views/events/events.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Icon, Image, Item, Label, Card } from 'semantic-ui-react'
-
-const paragraph = <Image src="https://react.semantic-ui.com/images/wireframe/short-paragraph.png" />
+import { Button, Icon, Item, Segment } from 'semantic-ui-react'
 
 export default class Events extends Component {
   /*
@@ -26,15 +24,11 @@ export default class Events extends Component {
 </script>*/
   componentDidMount() {
     window.EBWidgets.createWidget({
-      // Required
       widgetType: 'checkout',
       eventId: '76782042155',
-      iframeContainerId: 'eventbrite-widget-container-76782042155',
-
-      // Optional
-      iframeContainerHeight: 425, // Widget height in pixels. Defaults to a minimum of 425px if not provided
-      //iframeContainerHeight: 425, // Widget height in pixels. Defaults to a minimum of 425px if not provided
-      onOrderComplete: console.log('Complete') // Method called when an order has successfully completed
+      modal: true,
+      modalTriggerElementId: 'eventbrite-widget-modal-trigger-76782042155',
+      onOrderComplete: console.log('done')
     })
   }
   render() {
@@ -48,8 +42,17 @@ export default class Events extends Component {
               <Item.Header>11</Item.Header>
               <Item.Description>October</Item.Description>
             </Item.Content>
-            <Item.Content>
-              <div id="eventbrite-widget-container-76782042155"></div>
+            <Item.Content style={{ maxWidth: '700px' }}>
+              <div style={{ paddingBottom: '10px' }}>
+                <Segment>
+                  This is a test meeting this is where meeting description shows up. The RSVP button for this meeting
+                  section should work the ones below this are same duplicates.
+                </Segment>
+              </div>
+              <Button id="eventbrite-widget-modal-trigger-76782042155" primary floated="right">
+                RSVP
+                <Icon name="right chevron" />
+              </Button>
             </Item.Content>
           </Item>
 
@@ -60,18 +63,14 @@ export default class Events extends Component {
               <Item.Header>11</Item.Header>
               <Item.Description>October</Item.Description>
             </Item.Content>
-            <Item.Content>
-              <Item.Header>Meeting Topic is ABC</Item.Header>
-              <Item.Meta>
-                <span>AT ERB 315</span>
-              </Item.Meta>
-              <Item.Description>{paragraph}</Item.Description>
-              <Item.Extra>
-                <Button primary floated="right">
-                  RSVP
-                  <Icon name="right chevron" />
-                </Button>
-              </Item.Extra>
+            <Item.Content style={{ maxWidth: '700px' }}>
+              <div style={{ paddingBottom: '10px' }}>
+                <Segment>This is a test meeting this is where meeting description shows up.</Segment>
+              </div>
+              <Button id="eventbrite-widget-modal-trigger-76782042155" primary floated="right">
+                RSVP
+                <Icon name="right chevron" />
+              </Button>
             </Item.Content>
           </Item>
 
@@ -82,18 +81,14 @@ export default class Events extends Component {
               <Item.Header>11</Item.Header>
               <Item.Description>October</Item.Description>
             </Item.Content>
-            <Item.Content>
-              <Item.Header>Meeting Topic is ABC</Item.Header>
-              <Item.Meta>
-                <span>AT ERB 315</span>
-              </Item.Meta>
-              <Item.Description>{paragraph}</Item.Description>
-              <Item.Extra>
-                <Button primary floated="right">
-                  RSVP
-                  <Icon name="right chevron" />
-                </Button>
-              </Item.Extra>
+            <Item.Content style={{ maxWidth: '700px' }}>
+              <div style={{ paddingBottom: '10px' }}>
+                <Segment>This is a test meeting this is where meeting description shows up.</Segment>
+              </div>
+              <Button id="eventbrite-widget-modal-trigger-76782042155" primary floated="right">
+                RSVP
+                <Icon name="right chevron" />
+              </Button>
             </Item.Content>
           </Item>
         </Item.Group>
