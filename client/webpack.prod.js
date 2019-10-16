605c1eda2d63cf92483034f20548acc44d0fcf23
Nishad Aherrao <niaherrao@MB-NIAHERRAO.local>
2019-10-16 13:00:24 -0500

    Check

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
-        API_URI: JSON.stringify('')
+        API_URI: JSON.stringify('test')
       }
     })
   ]
