7ef0f6ab165b21ccb02d558a52f36fb3eeaec0d9
Nishad Aherrao <niaherrao@MB-NIAHERRAO.local>
2019-10-16 11:56:59 -0500

    Made officer images rescalable based on browser size so the image isnt too big for mobile

--- client/src/views/officers/officers.js
+++ client/src/views/officers/officers.js
@@ -6,111 +6,113 @@ class Officers extends Component {
     return (
       <div>
         <Grid style={{ fontSize: '1.5vw', backgroundColor: 'black', lineHeight: '2vw' }}>
-          <Grid.Row columns={'equal'} style={{ padding: '2vw' }}>
-            <Grid.Column style={{ maxWidth: '400px' }}>
-              <Card>
-                <Image src="/statics/jiang.jpg" wrapped ui={false} />
-                <Card.Content>
-                  <Card.Header>Jiang Ming</Card.Header>
-                  <Card.Meta style={{ paddingTop: '5px' }}>
-                    <span className="date">Joined in 2015</span>
-                  </Card.Meta>
-                  <Card.Description>Faculty Advisor</Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  <a>
-                    <Icon name="mail" />
-                    jiang.ming@uta.edu
-                  </a>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-            <Grid.Column style={{ alignSelf: 'center' }}>
-              <Segment inverted textAlign="left" style={{ fontSize: '2vw' }}>
-                I am an Assistant Professor in the Computer Science and Engineering Department at UT Arlington. My
-                research interests span cybersecurity and software engineering, with a focus on software security and
-                malware analysis. I am endeavoring to develop novel binary code analysis techniques to help people find
-                software vulnerabilities and defeat malicious software.
+          <Grid.Row style={{ padding: '2vw', display: 'flex' }}>
+            <div style={{ display: 'flex', width: '100%' }}>
+              <Segment inverted textAlign="left" style={{ fontSize: '2vw', display: 'flex', width: '100%' }}>
+                <div style={{ width: '15vw', fontSize: '1vw' }}>
+                  <Card>
+                    <Image src="/statics/jiang.jpg" wrapped ui={false} />
+                    <Card.Content>
+                      <Card.Header>Jiang Ming</Card.Header>
+                      <Card.Meta style={{ paddingTop: '5px' }}>
+                        <span className="date">Joined in 2015</span>
+                      </Card.Meta>
+                      <Card.Description>Faculty Advisor</Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <a>
+                        <Icon name="mail" />
+                        jiang.ming@uta.edu
+                      </a>
+                    </Card.Content>
+                  </Card>
+                </div>
+                <div style={{ alignSelf: 'center', margin: '5vw', paddingBottom: '5vw' }}>
+                  I am an Assistant Professor in the Computer Science and Engineering Department at UT Arlington. My
+                  research interests span cybersecurity and software engineering, with a focus on software security and
+                  malware analysis. I am endeavoring to develop novel binary code analysis techniques to help people
+                  find software vulnerabilities and defeat malicious software.
+                </div>
               </Segment>
-            </Grid.Column>
+            </div>
           </Grid.Row>
           <Divider inverted />
-          <Grid.Row columns={'equal'} style={{ padding: '2vw' }}>
-            <Grid.Column style={{ maxWidth: '400px' }}>
-              <Card>
-                <Image src="/statics/zehra.jpg" wrapped ui={false} />
-                <Card.Content>
-                  <Card.Header>Zehra Jafri</Card.Header>
-                  <Card.Meta style={{ paddingTop: '5px' }}>
-                    <span className="date">Joined in 2015</span>
-                  </Card.Meta>
-                  <Card.Description>President</Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  <a>
-                    <Icon name="mail" />
-                    zehra.jafri@mavs.uta.edu
-                  </a>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-            <Grid.Column style={{ alignSelf: 'center' }}>
-              <Segment inverted textAlign="left" style={{ fontSize: '2vw' }}>
-                I am Zehra.
+          <Grid.Row style={{ padding: '2vw', display: 'flex' }}>
+            <div style={{ display: 'flex', width: '100%' }}>
+              <Segment inverted textAlign="left" style={{ fontSize: '2vw', display: 'flex', width: '100%' }}>
+                <div style={{ width: '15vw', fontSize: '1vw' }}>
+                  <Card>
+                    <Image src="/statics/zehra.jpg" wrapped ui={false} />
+                    <Card.Content>
+                      <Card.Header>Zehra Jafri</Card.Header>
+                      <Card.Meta style={{ paddingTop: '5px' }}>
+                        <span className="date">Joined in 2015</span>
+                      </Card.Meta>
+                      <Card.Description>President</Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <a>
+                        <Icon name="mail" />
+                        zehra.jafri@mavs.uta.edu
+                      </a>
+                    </Card.Content>
+                  </Card>
+                </div>
+                <div style={{ alignSelf: 'center', margin: '5vw', paddingBottom: '5vw' }}>I am Zehra.</div>
               </Segment>
-            </Grid.Column>
+            </div>
           </Grid.Row>
           <Divider inverted />
-          <Grid.Row columns={'equal'} style={{ padding: '2vw' }}>
-            <Grid.Column style={{ maxWidth: '400px' }}>
-              <Card>
-                <Image src="/statics/nishad.jpg" wrapped ui={false} />
-                <Card.Content>
-                  <Card.Header>Nishad Aherrao</Card.Header>
-                  <Card.Meta style={{ paddingTop: '5px' }}>
-                    <span className="date">Joined in 2015</span>
-                  </Card.Meta>
-                  <Card.Description>Vice President</Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  <a>
-                    <Icon name="mail" />
-                    nishad.aherrao@mavs.uta.edu
-                  </a>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-            <Grid.Column style={{ alignSelf: 'center' }}>
-              <Segment inverted textAlign="left" style={{ fontSize: '2vw' }}>
-                I am Nishad.
+          <Grid.Row style={{ padding: '2vw', display: 'flex' }}>
+            <div style={{ display: 'flex', width: '100%' }}>
+              <Segment inverted textAlign="left" style={{ fontSize: '2vw', display: 'flex', width: '100%' }}>
+                <div style={{ width: '15vw', fontSize: '1vw' }}>
+                  <Card>
+                    <Image src="/statics/nishad.jpg" wrapped ui={false} />
+                    <Card.Content>
+                      <Card.Header>Nishad Aherrao</Card.Header>
+                      <Card.Meta style={{ paddingTop: '5px' }}>
+                        <span className="date">Joined in 2015</span>
+                      </Card.Meta>
+                      <Card.Description>Vice President</Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <a>
+                        <Icon name="mail" />
+                        nishad.aherrao@mavs.uta.edu
+                      </a>
+                    </Card.Content>
+                  </Card>
+                </div>
+                <div style={{ alignSelf: 'center', margin: '5vw', paddingBottom: '5vw' }}>I am Nishad.</div>
               </Segment>
-            </Grid.Column>
+            </div>
           </Grid.Row>
           <Divider inverted />
-          <Grid.Row columns={'equal'} style={{ padding: '2vw' }}>
-            <Grid.Column style={{ maxWidth: '400px' }}>
-              <Card>
-                <Image src="/statics/mario.jpg" wrapped ui={false} />
-                <Card.Content>
-                  <Card.Header>Mario</Card.Header>
-                  <Card.Meta style={{ paddingTop: '5px' }}>
-                    <span className="date">Joined in 2015</span>
-                  </Card.Meta>
-                  <Card.Description>Social Media</Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  <a>
-                    <Icon name="mail" />
-                    xyz@xyz
-                  </a>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-            <Grid.Column style={{ alignSelf: 'center' }}>
-              <Segment inverted textAlign="left" style={{ fontSize: '2vw' }}>
-                I am Mario.
+          <Grid.Row style={{ padding: '2vw', display: 'flex' }}>
+            <div style={{ display: 'flex', width: '100%' }}>
+              <Segment inverted textAlign="left" style={{ fontSize: '2vw', display: 'flex', width: '100%' }}>
+                <div style={{ width: '15vw', fontSize: '1vw' }}>
+                  <Card>
+                    <Image src="/statics/mario.jpg" wrapped ui={false} />
+                    <Card.Content>
+                      <Card.Header>Mario</Card.Header>
+                      <Card.Meta style={{ paddingTop: '5px' }}>
+                        <span className="date">Joined in 2015</span>
+                      </Card.Meta>
+                      <Card.Description>Social Media</Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <a>
+                        <Icon name="mail" />
+                        xyz@xyz
+                      </a>
+                    </Card.Content>
+                  </Card>
+                </div>
+                <div style={{ alignSelf: 'center', margin: '5vw', paddingBottom: '5vw' }}>I am Mario.</div>
               </Segment>
-            </Grid.Column>
+            </div>
           </Grid.Row>
           <Divider inverted />
         </Grid>
