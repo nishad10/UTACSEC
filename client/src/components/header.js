71fc7c11aa93a9e773eac9e7256f05f955688f84
Nishad Aherrao <niaherrao@MB-NIAHERRAO.local>
2019-10-17 20:35:45 -0500

    Changed header with menu items now can see which tab is active currently

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -4,10 +4,19 @@ import * as actions from '../actions'
 import { Link } from 'react-router-dom'
 import { Menu, Icon } from 'semantic-ui-react'
 class Header extends Component {
-  renderSignButton() {
+  constructor(props) {
+    super(props)
+    this.state = {}
+    this.handleItemClick = this.handleItemClick.bind(this)
+  }
+  handleItemClick(e, { name }) {
+    console.log(name)
+    this.setState({ activeItem: name })
+  }
+  renderSignButton(activeItem) {
     if (this.props.authenticated) {
       return (
-        <Menu size="tiny" icon="labeled">
+        <Menu icon="labeled">
           <Menu.Item as={Link} to="/signout" name="signout">
             <Icon name="sign-out" />
             SignOut
@@ -17,11 +26,23 @@ class Header extends Component {
     } else {
       return (
         <Menu.Menu position="right" inverted icon="labeled">
-          <Menu.Item as={Link} to="/signin" name="signin">
+          <Menu.Item
+            active={activeItem === 'signin'}
+            onClick={this.handleItemClick}
+            as={Link}
+            to="/signin"
+            name="signin"
+          >
             <Icon name="sign-in" />
             SignIn
           </Menu.Item>
-          <Menu.Item as={Link} to="/signup" name="signup">
+          <Menu.Item
+            active={activeItem === 'signup'}
+            onClick={this.handleItemClick}
+            as={Link}
+            to="/signup"
+            name="signup"
+          >
             <Icon name="signup" />
             SignUp
           </Menu.Item>
@@ -31,14 +52,33 @@ class Header extends Component {
   }
 
   render() {
+    const { activeItem } = this.state
     return (
-      <div display="flex">
-        <Menu inverted>
-          <Menu.Item as={Link} to="/" name="home" />
-          <Menu.Item as={Link} to="/events" name="events" />
-          <Menu.Item as={Link} to="/officers" name="officers" />
-          <Menu.Item as={Link} to="/account" name="account" />
-          {this.renderSignButton()}
+      <div display="flex" style={{ fontSize: '2vw' }}>
+        <Menu pointing secondary inverted>
+          <Menu.Item active={activeItem === 'home'} onClick={this.handleItemClick} as={Link} to="/" name="home" />
+          <Menu.Item
+            active={activeItem === 'events'}
+            onClick={this.handleItemClick}
+            as={Link}
+            to="/events"
+            name="events"
+          />
+          <Menu.Item
+            active={activeItem === 'officers'}
+            onClick={this.handleItemClick}
+            as={Link}
+            to="/officers"
+            name="officers"
+          />
+          <Menu.Item
+            active={activeItem === 'account'}
+            onClick={this.handleItemClick}
+            as={Link}
+            to="/account"
+            name="account"
+          />
+          {this.renderSignButton(activeItem)}
         </Menu>
       </div>
     )
