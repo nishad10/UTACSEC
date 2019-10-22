70c81beeb3490beb34a5b4e2a7298dab66004670
Nishad Aherrao <nishad.aherrao10@gmail.com>
2019-10-22 09:58:11 -0500

    Enable event rsvp

--- client/src/views/events/events.js
+++ client/src/views/events/events.js
@@ -52,55 +52,56 @@ export default class Events extends Component {
               style={{
                 textAlign: 'center',
                 fontSize: '3vw',
-                paddingRight: '2vw',
-                lineHeight: '4vw'
+                lineHeight: '4vw',
+                paddingRight: '3vw'
               }}
             >
-              <div style={{ alignSelf: 'center', paddingTop: '3vw', paddingBottom: '3vw', width: '20vw' }}>
+              <div style={{ alignSelf: 'center', paddingTop: '3vw', paddingBottom: '3vw', width: '17vw' }}>
                 <Item.Header style={{ fontSize: '4vw' }}>24th</Item.Header>
-                <Item.Description>October</Item.Description>
-                <Item.Description style={{ color: '#3cba45', fontSize: '2vw' }}>
-                  <div>
-                    Thursday<br></br>5pm-6pm
-                  </div>
+                <Item.Description style={{ fontSize: '2vw' }}>O C T</Item.Description>
+                <Item.Description style={{ color: '#3cba45', fontSize: '2vw', lineHeight: '3vw' }}>
+                  <div>Thursday</div>
+                  <div>5pm-6pm</div>
+                  <div>ERB 228</div>
                 </Item.Description>
               </div>
             </Item.Content>
 
             <Item.Content style={{ maxWidth: '700px', fontSize: '2vw', lineHeight: '3vw', paddingTop: '3vw' }}>
-              <div style={{ paddingBottom: '10px' }}>
-                The meeting will focus on use of metasploit and its syntax. Learn to use metasploit to discover
-                exploits, and validate vulnerabilities. By Zehra.
-                <p style={{ textDecoration: 'underline' }}>
-                  [ This weeks meeting will be held at ERB 228 not ERB 316! ]
-                </p>
+              <div style={{ paddingBottom: '10px', marginBottom: '1vw' }}>
+                The meeting will focus on use of metasploit and its syntax. Learn to use metasploit a penetration
+                testing framework to discover exploits, and validate vulnerabilities. - By Zehra Jafri.{' '}
               </div>
-              {renderConfirmNotConfirm(false)}
+              <div style={{ textDecoration: 'underline', marginBottom: '1.5vw' }}>
+                [ This weeks meeting will be held at ERB 228 not ERB 316! ]
+              </div>
+              {renderConfirmNotConfirm(true)}
             </Item.Content>
           </Item>
         </Segment>
+
         <Segment inverted>
           <Item style={{ display: 'flex' }}>
             <Item.Content
               style={{
                 textAlign: 'center',
                 fontSize: '3vw',
-                paddingRight: '2vw',
+                paddingRight: '3vw',
                 lineHeight: '4vw'
               }}
             >
-              <div style={{ alignSelf: 'center', paddingTop: '3vw', paddingBottom: '3vw', width: '20vw' }}>
+              <div style={{ alignSelf: 'center', paddingTop: '3vw', paddingBottom: '3vw', width: '17vw' }}>
                 <Item.Header style={{ fontSize: '4vw' }}>31st</Item.Header>
-                <Item.Description>October</Item.Description>
-                <Item.Description style={{ color: '#3cba45', fontSize: '2vw' }}>
-                  <div>
-                    Friday<br></br>7pm-9pm
-                  </div>
+                <Item.Description style={{ fontSize: '2vw' }}>O C T</Item.Description>
+                <Item.Description style={{ color: '#3cba45', fontSize: '2vw', lineHeight: '3vw' }}>
+                  <div>Friday</div>
+                  <div>7pm-9pm</div>
+                  <div>ERB 228</div>
                 </Item.Description>
               </div>
             </Item.Content>
             <Item.Content style={{ maxWidth: '700px', fontSize: '2vw', lineHeight: '3vw', paddingTop: '3vw' }}>
-              <div style={{ paddingBottom: '10px' }}>
+              <div style={{ paddingBottom: '10px', marginBottom: '1vw' }}>
                 The College of Engineering Halloween Party will be hosted by the UTA CSEC Club along with the help of a
                 few other COE clubs. You will be able to RSVP to the event and get your tickets soon on here. We will
                 send out an email when the tickets are available. Everything will be free.
