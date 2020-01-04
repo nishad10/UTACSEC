8e8dfd5a8d7dc8cba9beb7602b7ac3c34255c7fc
Nishad Aherrao <nishad.aherrao10@gmail.com>
2020-01-04 01:01:33 -0600

    Changes to enable discord account linking. (#17)
    * Add option to change discordid on acccounts page
    
    * Add option to enter discordid while signingup

--- client/src/views/auth/signup.js
+++ client/src/views/auth/signup.js
@@ -18,6 +18,7 @@ const Signup = props => {
   const [fname, setfName] = useState('')
   const [lname, setlName] = useState('')
   const [email, setEmail] = useState('')
+  const [discordID, setDiscordID] = useState('')
   const [password, setPassword] = useState('')
   const [success, setSuccess] = useState(true)
   const handleSubmit = () => {
@@ -25,79 +26,83 @@ const Signup = props => {
       firstName: fname,
       lastName: lname,
       email: email,
-      password: password
+      password: password,
+      discordID: discordID
     })
   }
-  useEffect(
-    () => {
-      if (error) {
-        setSuccess(false)
-      }
-    },
-    [error]
-  )
+  useEffect(() => {
+    if (error) {
+      setSuccess(false)
+    }
+  }, [error])
   return (
     <div>
       <Dimmer active={loading}>
         <Loader active={loading}>Signing You Up!</Loader>
       </Dimmer>
       <Grid
-        textAlign="center"
+        textAlign='center'
         style={{ height: '100vh' }}
-        verticalAlign="middle"
+        verticalAlign='middle'
       >
         <Grid.Column style={{ maxWidth: 450 }}>
           <Header
-            as="h2"
+            as='h2'
             style={{ color: 'white', fontSize: '2em' }}
-            textAlign="center"
+            textAlign='center'
           >
             Log-in to your account
           </Header>
-          <Form size="large" style={{ marginBottom: '1em' }} error={!success}>
+          <Form size='large' style={{ marginBottom: '1em' }} error={!success}>
             <Segment stacked>
               <Form.Input
                 onChange={e => setfName(e.target.value)}
                 fluid
-                icon="user"
-                iconPosition="left"
-                placeholder="First Name"
+                icon='user'
+                iconPosition='left'
+                placeholder='First Name'
               />
               <Form.Input
                 onChange={e => setlName(e.target.value)}
                 fluid
-                icon="user plus"
-                iconPosition="left"
-                placeholder="Last Name"
+                icon='user plus'
+                iconPosition='left'
+                placeholder='Last Name'
               />
               <Form.Input
                 onChange={e => setEmail(e.target.value)}
                 fluid
-                icon="at"
-                iconPosition="left"
-                placeholder="E-mail address"
+                icon='at'
+                iconPosition='left'
+                placeholder='E-mail address'
               />
               <Form.Input
                 onChange={e => setPassword(e.target.value)}
                 fluid
-                icon="lock"
-                iconPosition="left"
-                placeholder="Password"
-                type="password"
+                icon='lock'
+                iconPosition='left'
+                placeholder='Password'
+                type='password'
               />
-
+              <Form.Input
+                label='DiscordID'
+                value={discordID}
+                name='discordID'
+                placeholder='Enter your discord id.'
+                onChange={e => setDiscordID(e.target.value)}
+              />
               <Button
                 onClick={handleSubmit}
                 style={{ background: '#21ba45' }}
                 fluid
-                size="large"
+                size='large'
               >
                 Login
               </Button>
               <Message
                 error
-                header="Failed Login"
-                content="You already signed up with that email or signup not available"
+                header='Failed Login'
+                content='You already signed up with that email or signup not available'
               />
             </Segment>
           </Form>
